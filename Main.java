5d219de7d22a8472f97818822a478ed83a59e721
Bonnie <bonniec15@nycstudents.net>
2022-10-15 17:46:34 -0400

    Commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        int num =90;
+        System.out.println("The sin of " +  num + " degrees is " + Math.sin(Math.toRadians(num)));
+    }
+}
